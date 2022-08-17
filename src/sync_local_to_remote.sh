debbdf3203b6a996df63e17c618dd4359ffdc81c
Scott Teresi <teresiscott@gmail.com>
2022-08-17 15:30:02 -0400

    Add simple data sync commands

---
+++ src/sync_local_to_remote.sh
@@ -0,0 +1,2 @@
+rsync -ave ssh /home/scott/Documents/Uni/Research/Projects/Fish_TE_Differences/data --chmod=Dg+s teresisc@rsync.hpcc.msu.edu:/mnt/research/edgerpat_lab/Scotty/Fish_TE_Differences/
+rsync -ave ssh /home/scott/Documents/Uni/Research/Projects/Fish_TE_Differences/results --chmod=Dg+s teresisc@rsync.hpcc.msu.edu:/mnt/research/edgerpat_lab/Scotty/Fish_TE_Differences/
---
+++ src/sync_remote_to_local.sh
@@ -0,0 +1,1 @@
+rsync -ave ssh teresisc@rsync.hpcc.msu.edu:/mnt/research/edgerpat_lab/Scotty/Fish_TE_Differences/results /home/scott/Documents/Uni/Research/Projects/Fish_TE_Differences/
